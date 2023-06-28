16a7c70c7241d11d57051c8ddc24b257a11d612e
unknown <3236875710511@ingenieria.usac.edu.gt>
2023-06-28 16:49:01 -0600

    Modificando rutas

--- Backend/Functions/bucket.py
+++ Backend/Functions/bucket.py
@@ -78,13 +78,13 @@ class Bucket:
                 path)
 
         return msg
-    
-    #COMANDO COPY
-    @classmethod  
+
+    # COMANDO COPY
+    @classmethod
     def copyBucketBucket(self, origen, destino):
-        origen = self.get_absolute_path(origen)
-        destino = self.get_absolute_path(destino)
-        
+        origen = self.get_absolute_path_bucket(origen)
+        destino = self.get_absolute_path_bucket(destino)
+
         # Verificando existencia de las rutas
         existeOrigen = False
         objetos = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=origen)
@@ -101,11 +101,12 @@ class Bucket:
         if not existeDestino:
             print("La ruta de destino no existe en el bucket")
             return "La ruta de destino no existe en el bucket."
-        #Fin verificacion
-        
-        if origen.endswith('/'): #Si el origen es una carpeta
-            #Lista de objetos
-            objetos = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=origen) #Sustituir el nombre del bucket
+        # Fin verificacion
+
+        if origen.endswith('/'):  # Si el origen es una carpeta
+            # Lista de objetos
+            # Sustituir el nombre del bucket
+            objetos = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=origen)
 
             if 'Contents' not in objetos:
                 return "No se encontraron archivos en la carpeta de origen."
@@ -113,14 +114,17 @@ class Bucket:
             # Copiar cada archivo a la carpeta de destino
             for objeto in objetos['Contents']:
                 origen_archivo = objeto['Key']
-                destino_archivo = destino + origen_archivo.split('/')[-1]  #Construyendo la ruta de destino
-                s3.copy_object(Bucket=BUCKET_NAME, CopySource={'Bucket': BUCKET_NAME, 'Key': origen_archivo}, Key=destino_archivo)
+                # Construyendo la ruta de destino
+                destino_archivo = destino + origen_archivo.split('/')[-1]
+                s3.copy_object(Bucket=BUCKET_NAME, CopySource={
+                               'Bucket': BUCKET_NAME, 'Key': origen_archivo}, Key=destino_archivo)
             print("Copiados exitosamente")
             return "Los archivos han sido copiados exitosamente."
-        else: #Si el origen es un archivo
+        else:  # Si el origen es un archivo
             try:
                 s3.head_object(Bucket=BUCKET_NAME, Key=origen)
-                s3.copy_object(Bucket=BUCKET_NAME, CopySource={'Bucket': BUCKET_NAME, 'Key': origen}, Key=destino) #Copiando el archivo
+                s3.copy_object(Bucket=BUCKET_NAME, CopySource={
+                               'Bucket': BUCKET_NAME, 'Key': origen}, Key=destino)  # Copiando el archivo
                 print("Copia exitosa en la ruta" + destino)
                 return "Archivo copiado exitosamente."
             except s3.exceptions.ClientError as e:
@@ -128,14 +132,14 @@ class Bucket:
                     return "El archivo a copiar no existe."
                 else:
                     return "Se produjo un error al verificar el archivo de origen."
-                
+
     @classmethod
     def copyBucketServer(self, origen, destino):
-        #Enlazando las dos rutas a la carpeta raiz: Archivos
-        origen = self.get_absolute_path(origen) 
-        destino = self.get_absolute_path(destino)
-        
-        #Verificando que exista la ruta en el bucket
+        # Enlazando las dos rutas a la carpeta raiz: Archivos
+        origen = self.get_absolute_path_bucket(origen)
+        destino = self.get_absolute_path_server(destino)
+
+        # Verificando que exista la ruta en el bucket
         try:
             s3.head_object(Bucket=BUCKET_NAME, Key=origen)
         except s3.exceptions.ClientError as e:
@@ -145,52 +149,54 @@ class Bucket:
             else:
                 return "Se produjo un error al verificar la ruta de origen."
 
-        #Verificando si la ruta de destino en el server existe
+        # Verificando si la ruta de destino en el server existe
         if not os.path.exists(destino):
             print("La ruta de destino en la máquina virtual no existe.")
             return "La ruta de destino en la máquina virtual no existe."
 
-        if origen.endswith('/'): #La ruta de origen es de una carpeta
+        if origen.endswith('/'):  # La ruta de origen es de una carpeta
             objetos = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=origen)
 
             if 'Contents' not in objetos:
                 return "No se encontraron archivos en la carpeta de origen."
 
-            #Copiando cada archivo de la carpeta de origen
+            # Copiando cada archivo de la carpeta de origen
             for objeto in objetos['Contents']:
                 origenArchivo = objeto['Key']
                 nombreArchivo = origenArchivo.split('/')[-1]
-                destinoArchivo = os.path.join(destino, nombreArchivo) #Ruta de destino construida
-                s3.download_file(Bucket=BUCKET_NAME, Key=origenArchivo, Filename=destinoArchivo)
+                # Ruta de destino construida
+                destinoArchivo = os.path.join(destino, nombreArchivo)
+                s3.download_file(Bucket=BUCKET_NAME,
+                                 Key=origenArchivo, Filename=destinoArchivo)
             print("Los archivos han sido copiados exitosamente a la máquina virtual.")
             return "Los archivos han sido copiados exitosamente a la máquina virtual."
         else:
-            #La ruta de origen es de un archivo
+            # La ruta de origen es de un archivo
             if not os.path.isdir(destino):
                 return "La ruta de destino no existe en la máquina virtual"
 
-            #Obteniendo el nombre del archivo de origen
+            # Obteniendo el nombre del archivo de origen
             nombreArchivo = os.path.basename(origen)
 
-            #Construyendo la ruta de destino en el server
+            # Construyendo la ruta de destino en el server
             destinoArchivo = os.path.join(destino, nombreArchivo)
 
-            #Copiando el archivo
-            s3.download_file(Bucket=BUCKET_NAME, Key=origen, Filename=destinoArchivo)
+            # Copiando el archivo
+            s3.download_file(Bucket=BUCKET_NAME, Key=origen,
+                             Filename=destinoArchivo)
             print("El archivo ha sido copiado exitosamente a la máquina virtual.")
             return "El archivo ha sido copiado exitosamente a la máquina virtual."
 
-        
     @classmethod
     def copyServerBucket(self, origen, destino):
         s3 = boto3.client('s3')
-        origen = self.get_absolute_path(origen)
-        destino = self.get_absolute_path(destino)
-        #Verificando si la ruta de origen existe
+        origen = self.get_absolute_path_server(origen)
+        destino = self.get_absolute_path_bucket(destino)
+        # Verificando si la ruta de origen existe
         if not os.path.exists(origen):
             return "La ruta de origen en el server no existe. :("
 
-        #Verificando si la ruta destino existe
+        # Verificando si la ruta destino existe
         try:
             s3.head_object(Bucket=BUCKET_NAME, Key=destino)
         except s3.exceptions.ClientError as e:
@@ -200,27 +206,28 @@ class Bucket:
             else:
                 return "Se produjo un error al verificar la ruta de destino en el bucket."
 
-        if os.path.isdir(origen): 
-            #La ruta a copiar es de una carpeta
+        if os.path.isdir(origen):
+            # La ruta a copiar es de una carpeta
             archivos = os.listdir(origen)
             if not archivos:
                 return "No se encontraron archivos en la carpeta de origen."
 
-            #Copiando cada archivo en la carpeta hacia la carpeta destino
+            # Copiando cada archivo en la carpeta hacia la carpeta destino
             for archivo in archivos:
                 rutaArchivo = os.path.join(origen, archivo)
                 destinoArchivo = os.path.join(destino, archivo)
-                s3.upload_file(Filename=rutaArchivo, Bucket=BUCKET_NAME, Key=destinoArchivo)
+                s3.upload_file(Filename=rutaArchivo,
+                               Bucket=BUCKET_NAME, Key=destinoArchivo)
             print("Los archivos han sido copiados exitosamente al bucket.")
             return "Los archivos han sido copiados exitosamente al bucket."
         else:
-            #El origen es un archivo
+            # El origen es un archivo
             archivo = os.path.basename(origen)
             destinoArchivo = os.path.join(destino, archivo)
-            s3.upload_file(Filename=origen, Bucket=BUCKET_NAME, Key=destinoArchivo)
+            s3.upload_file(Filename=origen, Bucket=BUCKET_NAME,
+                           Key=destinoArchivo)
             print("El archivo ha sido copiado exitosamente al bucket.")
             return "El archivo ha sido copiado exitosamente al bucket."
-
 
     @classmethod
     def modify(self, path, body):
@@ -239,10 +246,10 @@ class Bucket:
             msg = 'Error! no se pudo modificar el contenido del archivo!'
         return msg
 
-    @classmethod            
+    @classmethod
     def rename(self, ruta, nuevoNombre):
-        ruta = self.get_absolute_path(ruta)
-        #Verificar si la ruta de origen existe
+        ruta = self.get_absolute_path_bucket(ruta)
+        # Verificar si la ruta de origen existe
         try:
             s3.head_object(Bucket=BUCKET_NAME, Key=ruta)
         except s3.exceptions.ClientError as e:
@@ -252,15 +259,15 @@ class Bucket:
             else:
                 return "Se produjo un error al verificar la ruta de origen."
 
-        #Extraer el nombre del archivo o carpeta actual
+        # Extraer el nombre del archivo o carpeta actual
         nombreActual = ruta.split('/')[-1]
-        #Extraer la ruta de la carpeta padre
+        # Extraer la ruta de la carpeta padre
         rutaPadre = ruta.rsplit('/', 1)[0]
-        #Construir la ruta de destino con el nuevo nombre
+        # Construir la ruta de destino con el nuevo nombre
         rutaDestino = rutaPadre + '/' + nuevoNombre
 
         try:
-            #Verificando si la ruta de destino ya existe
+            # Verificando si la ruta de destino ya existe
             s3.head_object(Bucket=BUCKET_NAME, Key=rutaDestino)
             print("Ya existe un archivo o carpeta con el nuevo nombre.")
             return "Ya existe un archivo o carpeta con el nuevo nombre."
@@ -268,15 +275,16 @@ class Bucket:
             if e.response['Error']['Code'] != '404':
                 return "Se produjo un error al verificar la ruta de destino."
 
-        #Renombrando el archivo o carpeta
+        # Renombrando el archivo o carpeta
         try:
-            s3.copy_object(Bucket=BUCKET_NAME, CopySource={'Bucket': BUCKET_NAME, 'Key': ruta}, Key=rutaDestino)  #Renombrar
+            s3.copy_object(Bucket=BUCKET_NAME, CopySource={
+                           'Bucket': BUCKET_NAME, 'Key': ruta}, Key=rutaDestino)  # Renombrar
             s3.delete_object(Bucket=BUCKET_NAME, Key=ruta)
             print("Se renombro con exito")
             return "Se renombró exitosamente."
         except s3.exceptions.ClientError as e:
             return "Se produjo un error al renombrar el archivo o carpeta."
-        
+
     @classmethod
     def transfer_server_bucket(self, from_path, to_path):
         msg = ''
@@ -487,24 +495,26 @@ class Bucket:
 
     @classmethod
     def backupServerToBucket(self, nombre_backup):
-        ruta_archivos = '/home/ubuntu/Archivos/' #Ruta de la carpeta archivos en el server
-        ruta_backup = '/home/ubuntu/' + nombre_backup + '/' #Ruta de la carpeta backup en el bucket
+        # Ruta de la carpeta archivos en el server
+        ruta_archivos = '/home/ubuntu/Archivos/'
+        ruta_backup = '/home/ubuntu/' + nombre_backup + \
+            '/'  # Ruta de la carpeta backup en el bucket
         try:
             s3.put_object(Bucket=BUCKET_NAME, Key=nombre_backup + '/')
 
-            #Se obtiene la lista de los archivos o carpetas
+            # Se obtiene la lista de los archivos o carpetas
             contenido = os.listdir(ruta_archivos)
-            
-            #Se sube cada archivo dentro de la carpeta del bucket
+
+            # Se sube cada archivo dentro de la carpeta del bucket
             for item in contenido:
                 ruta_item = os.path.join(ruta_archivos, item)
 
                 if os.path.isfile(ruta_item):
-                    #Se sube el archivo al bucket
+                    # Se sube el archivo al bucket
                     ruta_destino = nombre_backup + '/' + item
                     s3.upload_file(ruta_item, BUCKET_NAME, ruta_destino)
                 elif os.path.isdir(ruta_item):
-                    #Se sube todo el contenido de la carpeta en forma recursiva
+                    # Se sube todo el contenido de la carpeta en forma recursiva
                     subcarpeta = item + '/'
                     ruta_destino = nombre_backup + '/' + subcarpeta
                     subcontenido = os.listdir(ruta_item)
@@ -512,7 +522,8 @@ class Bucket:
                     for subitem in subcontenido:
                         ruta_subitem = os.path.join(ruta_item, subitem)
                         ruta_subdestino = ruta_destino + subitem
-                        s3.upload_file(ruta_subitem, BUCKET_NAME, ruta_subdestino)
+                        s3.upload_file(
+                            ruta_subitem, BUCKET_NAME, ruta_subdestino)
             print("Se ha hecho un backup del server en el bucket: " + BUCKET_NAME)
             return "Se ha hecho un backup del server en el bucket: " + BUCKET_NAME
         except Exception as e:
@@ -521,60 +532,70 @@ class Bucket:
 
     @classmethod
     def backupBucketToBucket(self, nombre_backup):
-        ruta_archivos = '/home/ubuntu/Archivos/'  #Ruta de la carpeta archivos en el bucket
-        ruta_backup = '/home/ubuntu/' + nombre_backup + '/'  #Ruta de la carpeta backup en el bucket
+        # Ruta de la carpeta archivos en el bucket
+        ruta_archivos = '/home/ubuntu/Archivos/'
+        ruta_backup = '/home/ubuntu/' + nombre_backup + \
+            '/'  # Ruta de la carpeta backup en el bucket
 
         try:
-            #Se obtiene la lista de archivos o carpetas
-            contenido = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=ruta_archivos)
+            # Se obtiene la lista de archivos o carpetas
+            contenido = s3.list_objects_v2(
+                Bucket=BUCKET_NAME, Prefix=ruta_archivos)
 
             for objeto in contenido['Contents']:
                 ruta_objeto = objeto['Key']
                 nombre_objeto = ruta_objeto.replace(ruta_archivos, '')
 
                 if nombre_objeto:
-                    #Se sube el objeto a la carpeta del backup
+                    # Se sube el objeto a la carpeta del backup
                     ruta_destino = nombre_backup + '/' + nombre_objeto
-                    s3.copy_object(Bucket=BUCKET_NAME, CopySource={'Bucket': BUCKET_NAME, 'Key': ruta_objeto}, Key=ruta_destino)
+                    s3.copy_object(Bucket=BUCKET_NAME, CopySource={
+                                   'Bucket': BUCKET_NAME, 'Key': ruta_objeto}, Key=ruta_destino)
             print("El backup se ha creado exitosamente en el bucket: " + BUCKET_NAME)
             return "El backup se ha creado exitosamente en el bucket: " + BUCKET_NAME
         except Exception as e:
             print("Error backup BucketToBucket: " + str(e))
             return "Error al realizar el backup: "
-        
+
     @classmethod
     def backupBucketToServer(self, nombre_backup):
-        ruta_archivos = '/home/ubuntu/Archivos/'  #Ruta de la carpeta archivos en el bucket
-        ruta_backup = '/home/ubuntu/' + nombre_backup + '/'  #Ruta de la carpeta backup en el server
+        # Ruta de la carpeta archivos en el bucket
+        ruta_archivos = '/home/ubuntu/Archivos/'
+        ruta_backup = '/home/ubuntu/' + nombre_backup + \
+            '/'  # Ruta de la carpeta backup en el server
         try:
-            #Creando la carpeta del backup en el server
+            # Creando la carpeta del backup en el server
             os.makedirs(ruta_backup)
-            #Obteniendo la lista de archivos y carpetas
-            contenido = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=ruta_archivos)
+            # Obteniendo la lista de archivos y carpetas
+            contenido = s3.list_objects_v2(
+                Bucket=BUCKET_NAME, Prefix=ruta_archivos)
 
             for objeto in contenido['Contents']:
                 ruta_objeto = objeto['Key']
                 nombre_objeto = ruta_objeto.replace(ruta_archivos, '')
 
                 if nombre_objeto:
-                    #Descargando el objeto y guardandolo en la carpeta del backup
+                    # Descargando el objeto y guardandolo en la carpeta del backup
                     ruta_destino = os.path.join(ruta_backup, nombre_objeto)
                     s3.download_file(BUCKET_NAME, ruta_objeto, ruta_destino)
-                    
-            print("El backup se ha creado exitosamente en la máquina virtual en la ruta: " + ruta_backup)
+
+            print(
+                "El backup se ha creado exitosamente en la máquina virtual en la ruta: " + ruta_backup)
             return "El backup se ha creado exitosamente en la máquina virtual en la ruta: " + ruta_backup
         except Exception as e:
             print("errorBackup BucketServer " + str(e))
             return "Error al realizar el backup."
 
     @classmethod
-    def delete_all(self):  
-        ruta = '/home/ubuntu/Archivos/' #Reemplazar con la ruta que estara en el bucket
-        objetos = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=ruta) #Obteniendo todos los objetos en el bucket
-        
-        if 'Contents' in objetos: #Si hay objetos en la carpeta
+    def delete_all(self):
+        ruta = '/home/ubuntu/Archivos/'  # Reemplazar con la ruta que estara en el bucket
+        # Obteniendo todos los objetos en el bucket
+        objetos = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=ruta)
+
+        if 'Contents' in objetos:  # Si hay objetos en la carpeta
             for objeto in objetos['Contents']:
-                s3.delete_object(Bucket=BUCKET_NAME, Key=objeto['Key']) #Borrando cada objeto de la carpeta
+                # Borrando cada objeto de la carpeta
+                s3.delete_object(Bucket=BUCKET_NAME, Key=objeto['Key'])
         print("Se ha vaciado la carpeta Archivos")
         return "Se ha vaciado la carpeta archivos."
 
